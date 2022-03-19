413089dc1651ca4b0a7144743f84ce1d377bb310
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-03-19 12:02:51 -0500

    photos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import coverImage from '../../assets/images/coverphoto';
+import coverImage from '../../assets/images/cover-photo/Cit';
 function About() {
   return (
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
-      {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
